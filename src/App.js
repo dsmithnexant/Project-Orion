f996e5a08bef6a52c9b81ecd5d2306b2e2eb679a
Dan Smith <dsmith@nexant.com>
2021-07-06 16:16:12 -0400

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ function App() {
        <h1>Project Orion</h1> 
        <p>-Welcome to the Project Orion HomePage-</p> 
       </header>
+      <AppBar position="static">
+      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+      </Tabs>
+    </AppBar>
+    <TabPanel value={value} index={0}>
+      Item One
+    </TabPanel>
+    <TabPanel value={value} index={1}>
+      Item Two
+    </TabPanel>
+    <TabPanel value={value} index={2}>
+      Item Three
+    </TabPanel>
     </div>
   );
 }
